d4a645d8c18370541912b455389e1e47b220e16c
Wu <fwu31@ford.com>
2021-05-28 16:50:06 +0800

    modify queueing logic

--- src/main/java/com/example/demo/aop/TokenAspect.java
+++ src/main/java/com/example/demo/aop/TokenAspect.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import java.util.concurrent.TimeUnit;
+
 @Aspect
 @Component
 public class TokenAspect {
@@ -19,21 +21,27 @@ public class TokenAspect {
 
     @Around("execution(* com.example.demo.controller.DataController.setData(..))")
     public Object checkLogin(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println(Queue.userQueue.toString());
+
+        // 1. 不在队列中的话，返回请登录
         String token = RequestContextHolder.currentRequestAttributes().getSessionId();
-        // 1. 看缓存中有没有这个token，如果没有的话，删除队列中这个token，返回请登录
-        if (!redisUtils.exists(token)) {
-            Queue.userQueue.remove(token);
+        if (!Queue.userQueue.contains(token)) {
             return IMOOCJSONResult.errorMsg("请登录");
         }
 
-        // 2. 如果有的话，看在队列中是第几个，如果不是就返回在队列中的index
+        // 2. 比对是不是队列中的第一位，是的话就续期
         Integer index = Queue.userQueue.indexOf(token);
-        if (index != 0) {
-            return IMOOCJSONResult.errorMsg("你现在排在第 " + index++ + " 位");
+        if (index == 0) {
+            redisUtils.set(token,"",30L, TimeUnit.SECONDS);
+        }else {
+            // 3. 不是的话，清理当前队列
+            token = Queue.userQueue.get(0);
+            if (!redisUtils.exists(token)) { // 这里的校验 就意味着第一位只能保留30秒
+                Queue.userQueue.remove(token);
+            }
+            return IMOOCJSONResult.errorMsg("你现在排在第 " + index + " 位");
         }
 
-        // 3. 如果是第一个的话，看看还剩下多长时间，如果还剩下30秒，就再存一次，续一下时间
-        System.out.printf("this is the first one in the queue " + token);
         return pjp.proceed();
     }
 }
