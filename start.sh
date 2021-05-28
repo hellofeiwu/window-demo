23280f13e57e6fb56553ddd9f44e223a2f58ad40
Wu <fwu31@ford.com>
2021-05-28 16:50:51 +0800

    add sever access script

---
+++ start.sh
@@ -0,0 +1,2 @@
+echo $PWD
+sh /home/chuchuang/window-demo/apache-tomcat-9.0.46/bin/startup.sh
---
+++ stopAndClean.sh
@@ -0,0 +1,3 @@
+sh /home/chuchuang/window-demo/apache-tomcat-9.0.46/bin/shutdown.sh
+rm /home/chuchuang/window-demo/apache-tomcat-9.0.46/webapp/demo.war
+rm -rf /home/chuchuang/window-demo/apache-tomcat-9.0.46/webapp/demo
