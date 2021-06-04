e1c8ec781755fc879729081a81f35bbfcbfdf127
Wu <fwu31@ford.com>
2021-06-04 16:30:25 +0800

    update script

--- stopAndClean.sh
+++ stopAndClean.sh
@@ -1,3 +1,4 @@
+echo $PWD
 sh /home/chuchuang/window-demo/apache-tomcat-9.0.46/bin/shutdown.sh
-rm /home/chuchuang/window-demo/apache-tomcat-9.0.46/webapp/demo.war
-rm -rf /home/chuchuang/window-demo/apache-tomcat-9.0.46/webapp/demo
+rm /home/chuchuang/window-demo/apache-tomcat-9.0.46/webapps/demo.war
+rm -rf /home/chuchuang/window-demo/apache-tomcat-9.0.46/webapps/demo
