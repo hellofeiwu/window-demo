6067ab02abd3f8b5fdcc5b8680375a6551ef468a
Wu <fwu31@ford.com>
2021-06-04 17:19:14 +0800

    add log 功能

--- src/main/java/com/example/demo/controller/DataController.java
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.utils.IMOOCJSONResult;
 import com.example.demo.utils.RedisUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +14,15 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 public class DataController {
+    public static final Logger log = LoggerFactory.getLogger(DataController.class);
+
     @Autowired
     private RedisUtils redisUtils;
 
     @PostMapping(value = "/setData")
     public IMOOCJSONResult setData(@RequestParam String key, @RequestParam String value) {
         redisUtils.set(key,value,60L, TimeUnit.SECONDS);
+        log.info("****** setData api is called, key is " + key + ", value is " + value + " ******");
         return IMOOCJSONResult.ok();
     }
 
